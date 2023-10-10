ea2b2644d2e4ca6045d1e0de8d27fcc8c7510c4f
Azat Yanberdin <azatdjon@yandex.ru>
2023-10-10 11:05:43 +0500

    add social auth, Py Social Auth, add server for HTTPS

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -128,4 +128,18 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.facebook.FacebookOAuth2',
     'cocial_core.backends.twitter.TwitterOAuth2',
+    'cocial_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_PIPELINE = [ 
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 ]
