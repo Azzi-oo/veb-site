53239feed8f90184a4ad6fd785862fda0bbbe0a0
Azat Yanberdin <azatdjon@yandex.ru>
2023-10-12 22:52:15 +0500

    add new features

--- images/views.py
+++ images/views.py
@@ -44,15 +44,15 @@ def image_like(request):
     image_id = request.POST.get('id')
     action = request.POST.get('action')
     if image_id and action:
-    try:
-        image = Image.objects.get(id=image_id)
-        if action == 'like':
-            image.users_like.add(request.user)
-        else:
-            image.users_like.remove(request.user)
-        return JsonResponse({'status': 'ok'})
-    except Image.DoesNotExist:
-        pass
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
     return JsonResponse({'status': 'error'})
 
 
